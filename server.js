f535be8f133d6b10751ec39900fc87304dd4ec5e
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-03-08 09:16:36 -0800

    set-up passport and express-session

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
 const pug = require('pug');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
@@ -14,9 +16,19 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/').get((req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Hello', message: 'Please login' });
 });
 
 const PORT = process.env.PORT || 3000;
